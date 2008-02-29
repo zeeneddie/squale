b2d29ac7d76cce12679bc10c132479adad682524
Bertrand Franchet <bfranchet@quale.org>
2008-02-29 17:15:29 +0000

    Modification for the basic authentication

--- Squale/squale-web/src/main/java/com/airfrance/squaleweb/connection/IUserBean.java
+++ Squale/squale-web/src/main/java/com/airfrance/squaleweb/connection/IUserBean.java
@@ -1,14 +1,27 @@
 package com.airfrance.squaleweb.connection;
 
+import javax.servlet.http.HttpSession;
+
 /**
- * Bean utilisateur
+ * user bean
  */
 public interface IUserBean {
+	
+	
     
-    /** 
-     * Indique si l'utilisateur est administrateur SQUALE 
-     * @return true si l'utilisateur est administrateur SQUALE
-     */
+	/**
+	 * This method indicate if the authenticated user is a squale administrator 
+	 * @return return true if the authenticated user is a squale administrator
+	 */
     public boolean isAdmin();
+    
+    /**
+     * This method return the identifier of the userBean
+     * @return send the identifier of the userBean
+     */
+    public String getIdentifier();
+    
+    
+    
 
 }
--- Squale/squale-web/src/main/java/com/airfrance/squaleweb/connection/IUserBeanAccessor.java
+++ Squale/squale-web/src/main/java/com/airfrance/squaleweb/connection/IUserBeanAccessor.java
@@ -1,19 +1,50 @@
 package com.airfrance.squaleweb.connection;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import com.airfrance.jraf.commons.exception.JrafEnterpriseException;
+import com.airfrance.squalecommon.datatransfertobject.component.UserDTO;
 import com.airfrance.squaleweb.connection.exception.ConnectionException;
 
 /**
- * Interface pour accéder au bean utilisateur lors de la connexion
+ * user Bean Access interface
  */
 public interface IUserBeanAccessor {
     
     /**
-     * @param pRequest la requête permettant de récupérer le bean
-     * @return l'utilisateur connecté
-     * @throws ConnectionException si erreur de connexion
+     * This method return the userBean of the bean accessor
+     * @param pRequest the request used
+     * @return the authenticated userBean of the bean accessor
+     * @throws ConnectionException 
      */
     public IUserBean getUserBean(HttpServletRequest pRequest) throws ConnectionException;
+    
+    /**
+     * This method return the userBean of the bean accessor
+     * @return The userBean of the bean accessor
+     */
+    public IUserBean getUserBean();
+    
+    
+    /**
+     * This method do the authentication of the user.
+     * @param user object with only the identify and the password fill
+     * @return true if the user is recognized
+     */
+    public boolean isUser (UserDTO user);
+    
+    /**
+     * This method indicate if the user is connected to the application or not
+     * @return true if the user is connected
+     */
+    public boolean isConnect();
+    
+    
+    
+    /**This method disconnect the user from the application
+     * @param session The session in used
+     * */
+    public void disConnect(HttpSession session);
 
 }
