94e85d51f4f2ab9df4e095412dd3cad30c5da8e8
Fabrice Bellingard <bellingard@gmail.com>
2008-11-12 17:19:42 +0000

    Fixes some minor errors linked to [#2]

--- Squale/squale-database/oracle/migration-scripts/current-migration-script.sql
+++ Squale/squale-database/oracle/migration-scripts/current-migration-script.sql
@@ -3,14 +3,14 @@
 -- Need at least Oracle 9
 
 -- table ProjectParameter
-alter table ProjectParameter rename column skey to IndexKey;
+alter table ProjectParameter rename column key to IndexKey;
 
 -- Table News
-alter table News rename column skey to NewsKey;
+alter table News rename column key to NewsKey;
 
 -- Table Message
 alter table Message drop primary key;
-alter table Message rename column skey to MessageKey;
+alter table Message rename column key to MessageKey;
 alter table Message add constraint pk_messages primary key(MessageKey,lang);
 
 
