cc6f19002471aeb2db46b91f395238c9ac45f3f5
Fabrice Bellingard <bellingard@gmail.com>
2009-01-05 13:26:04 +0000

    [#120] Add Licensing information https://project.squale.org/ticket/120

--- Squale/squale-core/src/main/java/com/airfrance/squalecommon/enterpriselayer/businessobject/result/roi/RoiMetricsBO.java
+++ Squale/squale-core/src/main/java/com/airfrance/squalecommon/enterpriselayer/businessobject/result/roi/RoiMetricsBO.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2008-2010, Squale Project - http://www.squale.org
+ *
+ * This file is part of Squale.
+ *
+ * Squale is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * Squale is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Squale.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.airfrance.squalecommon.enterpriselayer.businessobject.result.roi;
 
 import com.airfrance.squalecommon.enterpriselayer.businessobject.result.IntegerMetricBO;
