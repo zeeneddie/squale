ff93b507b9a753db9d334176b3b178b9c94aef71
Fabrice Bellingard <bellingard@gmail.com>
2009-03-27 10:20:52 +0000

    [#150] Autocomplete for tag proposal triggers only after 2 characters

--- Squale/squale-web/src/main/java/com/airfrance/squaleweb/applicationlayer/action/component/ManageProjectAction.java
+++ Squale/squale-web/src/main/java/com/airfrance/squaleweb/applicationlayer/action/component/ManageProjectAction.java
@@ -19,64 +19,25 @@
 package com.airfrance.squaleweb.applicationlayer.action.component;
 
 import java.io.IOException;
-import java.text.MessageFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.ActionError;
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
 
 import com.airfrance.jraf.commons.exception.JrafEnterpriseException;
 import com.airfrance.jraf.helper.AccessDelegateHelper;
 import com.airfrance.jraf.spi.accessdelegate.IApplicationComponent;
-import com.airfrance.squalecommon.datatransfertobject.component.ApplicationConfDTO;
-import com.airfrance.squalecommon.datatransfertobject.component.AuditDTO;
-import com.airfrance.squalecommon.datatransfertobject.component.ProjectConfDTO;
 import com.airfrance.squalecommon.datatransfertobject.tag.TagDTO;
-import com.airfrance.squalecommon.enterpriselayer.businessobject.component.ApplicationBO;
-import com.airfrance.squalecommon.enterpriselayer.businessobject.component.AuditBO;
-import com.airfrance.squalecommon.enterpriselayer.businessobject.component.ProjectBO;
-import com.airfrance.squalecommon.enterpriselayer.businessobject.profile.ProfileBO;
-import com.airfrance.squalecommon.util.SqualeCommonConstants;
-import com.airfrance.squalecommon.util.SqualeCommonUtils;
-import com.airfrance.squalecommon.util.mail.MailerHelper;
-import com.airfrance.squaleweb.applicationlayer.action.ActionUtils;
 import com.airfrance.squaleweb.applicationlayer.action.accessRights.ReaderAction;
-import com.airfrance.squaleweb.applicationlayer.formbean.LogonBean;
-import com.airfrance.squaleweb.applicationlayer.formbean.component.AuditForm;
-import com.airfrance.squaleweb.applicationlayer.formbean.config.ServeurListForm;
-import com.airfrance.squaleweb.applicationlayer.formbean.creation.ApplicationRightsForm;
-import com.airfrance.squaleweb.applicationlayer.formbean.creation.CreateApplicationForm;
-import com.airfrance.squaleweb.applicationlayer.formbean.creation.CreateProjectForm;
-import com.airfrance.squaleweb.resources.WebMessages;
-import com.airfrance.squaleweb.transformer.ApplicationConfTransformer;
-import com.airfrance.squaleweb.transformer.AuditTransformer;
-import com.airfrance.squaleweb.transformer.ProjectConfTransformer;
-import com.airfrance.squaleweb.transformer.ServeurListTransformer;
-import com.airfrance.squaleweb.util.SqualeWebActionUtils;
 import com.airfrance.welcom.struts.ajax.WHttpEasyCompleteResponse;
-import com.airfrance.welcom.struts.bean.WActionForm;
 import com.airfrance.welcom.struts.easycomplete.WEasyCompleteUtil;
-import com.airfrance.welcom.struts.transformer.WTransformerException;
-import com.airfrance.welcom.struts.transformer.WTransformerFactory;
-import com.airfrance.welcom.struts.util.WConstants;
 
 /**
+ * Struts action used to manage tags.
  */
 public class ManageProjectAction
     extends ReaderAction
@@ -92,11 +53,10 @@ public class ManageProjectAction
      * @param response the servlet response.
      * @return null (because the response is an XML stream)
      */
+    @SuppressWarnings("unchecked")
     public ActionForward findTagForAutocomplete( final ActionMapping mapping, final ActionForm form,
                                                  final HttpServletRequest request, final HttpServletResponse response )
     {
-        // TODO: finish this up
-
         // retrieves the string that the user has just typed in
         String stringFirstChars = request.getParameter( "ch" );
         stringFirstChars = WEasyCompleteUtil.filter( stringFirstChars );
@@ -104,36 +64,23 @@ public class ManageProjectAction
         // create the response object
         WHttpEasyCompleteResponse easyComplete = new WHttpEasyCompleteResponse( response );
 
-        // and fill it with the users' information
-
-        /* -------------------------------------------------------------- */
-        // This is a example code snippet used to test the suggest field
-        // This must be encapsulated in a wider generic security API
-        /* -------------------------------------------------------------- */
-        // searching Tag is available only when the string is 3 characters long
-        if ( stringFirstChars.length() > 2 )
+        IApplicationComponent ac;
+        try
         {
-            IApplicationComponent ac;
-            try
+            ac = AccessDelegateHelper.getInstance( "TagAdmin" );
+            Object[] paramIn = { new String[] { stringFirstChars } };
+            Collection<TagDTO> tags = ( (Collection<TagDTO>) ac.execute( "getTagsByName", paramIn ) );
+            for ( TagDTO tagDTO : tags )
             {
-                ac = AccessDelegateHelper.getInstance( "TagAdmin" );
-                Object[] paramIn = { new String[] { stringFirstChars } };
-                Collection<TagDTO> tags = ( (Collection<TagDTO>) ac.execute( "getTagsByName", paramIn ) );
-                for ( TagDTO tagDTO : tags )
-                {
-                    String value = tagDTO.getName();
-                    String label = tagDTO.getName();
-                    easyComplete.addValueLabel( value, label );
-                }
-            }
-            catch ( JrafEnterpriseException e )
-            {
-                e.printStackTrace();
+                String value = tagDTO.getName();
+                String label = tagDTO.getName();
+                easyComplete.addValueLabel( value, label );
             }
         }
-
-        // TODO : need to see why only the 10 first results are displayed...
-        /* ------------------------------------------------------ */
+        catch ( JrafEnterpriseException e )
+        {
+            e.printStackTrace();
+        }
 
         try
         {
