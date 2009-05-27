f8978296349350f6c73f0315db488e3279cdf63e
Fabrice Bellingard <bellingard@gmail.com>
2009-05-27 14:56:45 +0000

    [#175] Add support for Derby database https://project.squale.org/ticket/175

---
+++ Squale/squale-database/derby/creation-scripts/01-createDatabase.sql
@@ -0,0 +1,4 @@
+CREATE TABLE JediKnight
+(User_Id char(50),
+First_Name char(50),
+Last_Name char(50))
---
+++ Squale/squale-database/derby/creation-scripts/03-initSquale.sql
@@ -0,0 +1,6 @@
+Insert into JediKnight
+ Values
+   ('B123456', 'Obiwan', 'Kenobi');
+Insert into JediKnight
+ Values
+   ('A987654', 'Anakin', 'Skywalker');
