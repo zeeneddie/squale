2c298ae5df2bcaf292ba06f1b031ea87c3857237
Fabrice Bellingard <bellingard@gmail.com>
2009-06-09 15:08:37 +0000

    [#175] Add support for Derby database Remove Derby files as this issue was closed as "wontfix"

--- Squale/squale-database/derby/creation-scripts/01-createDatabase.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE JediKnight
-(User_Id char(50),
-First_Name char(50),
-Last_Name char(50))
--- Squale/squale-database/derby/creation-scripts/03-initSquale.sql
+++
@@ -1,6 +0,0 @@
-Insert into JediKnight
- Values
-   ('B123456', 'Obiwan', 'Kenobi');
-Insert into JediKnight
- Values
-   ('A987654', 'Anakin', 'Skywalker');
