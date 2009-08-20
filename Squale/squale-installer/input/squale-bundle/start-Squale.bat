87669439c9c2c203e63a2765d667d342064863a8
Bertrand Franchet <bfranchet@quale.org>
2009-08-20 09:04:49 +0000

    [#207] This commit enable to start squale install on windows with the installer in a directory whose name contains a space

--- Squale/squale-installer/input/squale-bundle/start-Squale.bat
+++ Squale/squale-installer/input/squale-bundle/start-Squale.bat
@@ -3,10 +3,10 @@ set LANG=fr_FR.ISO8859-1
 set INSTALL_FOLDER=%cd%
 set SQUALE_HOME=%cd%\squale.home
 set CATALINA_HOME=%cd%\server
-set JAVA_OPTS=-Dsquale.home=%SQUALE_HOME%
+set JAVA_OPTS=-Dsquale.home="%SQUALE_HOME%"
 
 REM Ensure the logs directory exists for Squale Web
-if not exist %SQUALE_HOME%\SqualeWeb\logs mkdir %SQUALE_HOME%\SqualeWeb\logs
+if not exist "%SQUALE_HOME%\SqualeWeb\logs" mkdir "%SQUALE_HOME%\SqualeWeb\logs"
 
 REM First, start HSQL
 cd %INSTALL_FOLDER%\database
