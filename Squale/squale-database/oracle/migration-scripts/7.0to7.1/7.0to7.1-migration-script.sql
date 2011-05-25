b3691de488e035994a048886f24426910915f979
Bertrand Franchet <bfranchet@quale.org>
2011-05-25 14:30:01 +0000

    Preparing release 7.1

---
+++ Squale/squale-database/oracle/migration-scripts/7.0to7.1/7.0to7.1-migration-script.sql
@@ -0,0 +1,5 @@
+--#############################################
+-- Modify the squale version
+alter table AUDITBO modify SQUALE_VERSION default '7.1' ;
+
+--###########################################################
